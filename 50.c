56f8e8988f18c9e05eee62d328f316182102f7c0
Abhigyan Shekhar <abhishanu07@gmail.com>
2025-11-08 12:25:27 +0530

    Implement myPow function for power calculation

---
+++ 50.c
@@ -0,0 +1,19 @@
+double myPow(double x, int n) {
+    double result = 1.0;
+    long long power = n;  
+
+    if (power < 0) {
+        x = 1 / x;
+        power = -power;
+    }
+
+    while (power > 0) {
+        if (power % 2 == 1) {  
+            result *= x;       
+            power--;           
+        } else {               
+            x *= x;            
+            power /= 2;        
+        }
+    }
+    return result;
